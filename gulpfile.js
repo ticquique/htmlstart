90bcab8c054251413e0bc995afc238bac5eb1db8
quique <quiquepb_94@hotmail.com>
2018-01-29 23:54:44 +0100

    Reload on change html files in all directories and all scss inside scss folder

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,13 @@ gulp.task('serve', ['sass'], function() {
         server: "."
     });
 
-    gulp.watch("scss/*.scss", ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("scss/**/*.scss", ['sass']);
+    gulp.watch("**/*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("scss/*.scss")
+    return gulp.src("scss/**/*.scss")
         .pipe(sass())
         .pipe(gulp.dest("css"))
         .pipe(browserSync.stream());
